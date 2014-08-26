fd3c18e6dfb97a359b7953aaf44e249378b06f95
bizzk3t <bizzk3t@gmail.com>
2014-08-26 14:31:06 -0500

    Update musicplayer.js

--- musicplayer.js
+++ musicplayer.js
@@ -1,3 +1,26 @@
+/*
+    MusicPlayerJS is a audio player for the standard HTML file formats.
+    It is intended to improve customization of the standard audio player.
+
+    Copyright (C) 2014  William Harrison
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/    
+
+
+
 function addPlayer(id, songFile, song, artist, colors)
 {
     var colorBackground = colors[0];
